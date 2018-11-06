244092d37f22927c34ea3d5d0fe2f611ed3fc3c9
sulaiman13 <sulaiman.tsany@gmail.com>
2018-11-06 15:59:40 +0800

    first commit

---
+++ Assets/Scripts/Managers/KillManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class KillManager : MonoBehaviour
+{
+    public static int kill;
+
+
+    Text text;
+
+
+    void Awake ()
+    {
+        text = GetComponent <Text> ();
+        kill = 0;
+    }
+
+
+    void Update ()
+    {
+        text.text = "KILL: " + kill;
+    }
+}
---
+++ Assets/Scripts/Managers/TimerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TimerManager : MonoBehaviour {
+
+	public static float time;
+	PlayerHealth playerHealth;
+	string minutes;
+	string seconds;
+
+    Text text;
+
+    void Awake ()
+    {
+        text = GetComponent <Text> ();
+        time = 0;
+		playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+
+    void Update ()
+    {
+		if (playerHealth.IsDead()) {
+			return;
+		}
+		time += Time.deltaTime;
+		minutes = Mathf.Floor(time/60).ToString("00");
+		seconds = (time % 60).ToString("00");
+        text.text = string.Format("{0}:{1}", minutes, seconds);
+    }
+}
